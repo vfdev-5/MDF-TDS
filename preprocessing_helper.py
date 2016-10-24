a4a56ad93021677020e751eb4d00ad021a7de2aa
vfdev-5 <vfdev.5@gmail.com>
2016-10-24 16:59:34 +0100

    * Refactor between helpers

--- preprocessing_helper.py
+++ preprocessing_helper.py
@@ -39,7 +39,52 @@ def get_unique_noconst(df, verbose=False):
     return df_unique_noconst
 
             
-    
+def split_series_values(series, splitter=','):
+    """
+    Method returns a pd.Series of splitted input values given as pd.Series
+    Input should be non-numerical
+    For example, ['a', 'a,b', 'b', 'a,b,c'] -> ['a', 'a', 'b', 'b', 'a', 'b', 'c']
+    """
+    res = series.apply(lambda val: val.split(splitter)).values
+    ret = []
+    for a in res:
+        ret.extend(a)
+    return pd.Series(ret)    
     
 
-
+def get_unique_col_values(df, display_nb_cols=10):
+    """
+    Method returns a DataFrame with columns and their unique values.
+    Use for non-numeric columns only.
+    
+    For example, input dataframe :
+        A    B    C 
+        v1   w1   u1
+        v1   w2   u2
+        v2   w2   u3
+        v3   w2   u3
+    
+    Output dataframe is :
+    
+        A  3  v1    v2    v3
+        B  2  w1    w2
+        C  3  u1    u2    u3
+        
+    """
+    nonnum_cols = df.columns
+    max_nb_cols = 0
+    rows = []
+    for i, col_name in enumerate(nonnum_cols):
+        ll = df[col_name].unique().tolist()
+        row = [col_name, len(ll)]
+        row.extend(ll)
+        max_nb_cols = max(max_nb_cols, len(row))
+        rows.append(row)
+        
+    out_df = pd.DataFrame(columns=range(max_nb_cols), index=range(len(nonnum_cols)))    
+    for i in range(len(out_df)):
+        row = rows[i]
+        if len(row) < max_nb_cols:
+            row.extend([""]*(max_nb_cols - len(row)))
+        out_df.iloc[i] = row
+    return out_df
